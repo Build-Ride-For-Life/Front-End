2e3df5274ac0fc6f2d7c88a60a286af7ad0adc98
William Ryan <billy.ryan.rom@gmail.com>
2020-02-04 14:02:26 -0500

    Refractor Proper Syntax to Users/Drivers Login and Signup

--- ride-for-life/src/components/DriverOnboarding.js
+++ ride-for-life/src/components/DriverOnboarding.js
@@ -3,11 +3,11 @@ import {useForm} from "react-hook-form";
 
 export default function DriverOnboarding() {
 
-    const [newDriver, setNewDriver] = useState({
-        name: "",
-        email: "",
-        password: "",
-    })
+    // const [newDriver, setNewDriver] = useState({
+    //     name: "",
+    //     email: "",
+    //     password: "",
+    // })
 
     const { register, handleSubmit, errors } = useForm();
     const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -20,49 +20,49 @@ export default function DriverOnboarding() {
         <form className="DriverSignUp" onSubmit={handleSubmit(onSubmit)}>
             <h1>New Driver Sign Up</h1>
             <label>Name:</label>
-            <input name="name" value={newDriver.name} ref={register({ required: true })} />
-            {errors.name && errors.name.type === "required" && (
+            <input name="drivers_name" ref={register({ required: true })} />
+            {errors.drivers_name && errors.drivers_name.type === "required" && (
                 <p>This is required</p>
             )}
 
             <label>Email:</label>
-            <input name="email" type="email" value={newDriver.email} ref={register({ required: true })} />
-            {errors.email && errors.email.type === "required" && (
+            <input name="drivers_email" type="email" ref={register({ required: true })} />
+            {errors.drivers_email && errors.drivers_email.type === "required" && (
                 <p>This is required</p>
             )}
 
             <label>Phone Number:</label>
-            <input name="number" type="text" ref={register({ required: true })} />
-            {errors.number && errors.number.type === "required" && (
+            <input name="drivers_phone_number" type="text" ref={register({ required: true })} />
+            {errors.drivers_phone_number && errors.drivers_phone_number.type === "required" && (
                 <p>This is required</p>
             )}
 
             <label>Address:</label>
-            <input name="address" type="text" ref={register({ required: true })} />
-            {errors.address && errors.address.type === "required" && (
+            <input name="drivers_plot" type="text" ref={register({ required: true })} />
+            {errors.drivers_plot && errors.drivers_plot.type === "required" && (
                 <p>This is required</p>
             )}
 
             <label>Price:</label>
-            <input name="price" type="text" ref={register({ required: true })} />
-            {errors.price && errors.price.type === "required" && (
+            <input name="drivers_price" type="text" ref={register({ required: true })} />
+            {errors.drivers_price && errors.drivers_price.type === "required" && (
                 <p>This is required</p>
             )}
 
 
             <label>Password:</label>
-            <input name="password" type="password" value={newDriver.password} ref={register({ required: true })} />
+            <input name="password" type="password" ref={register({ required: true })} />
             {errors.password && errors.password.type === "required" && (
                 <p>This is required</p>
             )}
 
             <label>Re-enter Password:</label>
-            <input name="enter" type="password" value={newDriver.password} ref={register({ required: true })} />
-            {errors.enter && errors.enter.type === "required" && (
+            <input name="password" type="password" ref={register({ required: true })} />
+            {errors.password && errors.password.type === "required" && (
                 <p>This is required</p>
             )}
 
-            <input name="submit"/>
+            <button type="submit">Submit</button>
         </form>
     )
 }
