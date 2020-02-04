63c3a57426ed692dc5455c8618c751d809cef5cd
William Ryan <billy.ryan.rom@gmail.com>
2020-02-03 19:05:14 -0500

    Add Login/Sign up for Drivers/Add Login/Sign up for Users

---
+++ ride-for-life/src/components/UserLogin.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import useForm from "react-hook-form";
+
+export default function UserLogin() {
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const { register, handleSubmit, errors } = useForm();
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const onSubmit = data => {
+        console.log(data);
+    };
+    const validateData = async (value) => {};
+
+    return (
+        <form className="UserLogin" onSubmit={handleSubmit(onSubmit)}>
+            <h1>Driver Log in</h1>
+            <label>Name:</label>
+            <input name="name" value={user.name} ref={register({ required: true })} />
+            {errors.name && errors.name.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Email:</label>
+            <input name="email" type="email" value={user.email} ref={register({ required: true })} />
+            {errors.email && errors.email.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Password:</label>
+            <input name="password" type="password" value={user.password} ref={register({ required: true })} />
+            {errors.password && errors.password.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <input name="submit"/>
+        </form>
+    )
+}
---
+++ ride-for-life/src/components/UserOnboarding.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import useForm from "react-hook-form";
+
+export default function UserOnboarding() {
+
+    const [newUser, setNewUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    })
+
+    const { register, handleSubmit, errors } = useForm();
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    const onSubmit = data => {
+        console.log(data);
+    };
+    const validateData = async (value) => {};
+
+    return (
+        <form className="UserSignUp" onSubmit={handleSubmit(onSubmit)}>
+            <h1>New User Sign Up</h1>
+            <label>Name:</label>
+            <input name="name" value={newUser.name} ref={register({ required: true })} />
+            {errors.name && errors.name.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Email:</label>
+            <input name="email" type="email" value={newUser.email} ref={register({ required: true })} />
+            {errors.email && errors.email.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Phone Number:</label>
+            <input name="number" type="text" ref={register({ required: true })} />
+            {errors.number && errors.number.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Address:</label>
+            <input name="address" type="text" ref={register({ required: true })} />
+            {errors.address && errors.address.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Expected Due Date:</label>
+            <input name="date" type="date" ref={register({ required: true })} />
+
+            <label>Password:</label>
+            <input name="password" type="password" value={newUser.password} ref={register({ required: true })} />
+            {errors.password && errors.password.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <label>Re-enter Password:</label>
+            <input name="enter" type="password" value={newUser.password} ref={register({ required: true })} />
+            {errors.enter && errors.enter.type === "required" && (
+                <p>This is required</p>
+            )}
+
+            <input name="submit"/>
+        </form>
+    )
+}
