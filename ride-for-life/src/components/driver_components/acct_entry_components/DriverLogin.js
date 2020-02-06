2101146f787a8c2f0d5c00de3a9f27c4244b4562
William Ryan <billy.ryan.rom@gmail.com>
2020-02-06 04:42:12 -0500

    Add Styling to Driver Login/Signup

--- ride-for-life/src/components/driver_components/acct_entry_components/DriverLogin.js
+++ ride-for-life/src/components/driver_components/acct_entry_components/DriverLogin.js
@@ -11,6 +11,17 @@ import {
     loginDriver,
     loginUser
 } from "../../../actions";
+import styled from "styled-components"
+import Navigation from "../../general_components/Navigation"
+
+const DriverForm = styled.form`
+display: flex;
+flex-direction: column;
+width: 31%;
+flex-wrap: wrap;
+margin: 9% auto 0%;
+background: rgb(88, 110, 107);
+`;
 
 function DriverLogin(props) {
 
@@ -23,23 +34,27 @@ function DriverLogin(props) {
     // const validateData = async (value) => {};
 
     return (
-        <form className="DriverLogin" onSubmit={handleSubmit(onSubmit)}>
-            <h1>Driver Log in</h1>
+        <div>
+            <DriverForm className="DriverLogin" onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <h1 style={{margin: `0% 0% 5%`, border: `3px solid`}}>Driver Log in</h1>
+                </div>
+                <div style={{margin:`0% 0% 3%`}}>
+                    <input name="drivers_email" type="email" placeholder="Email" style={{margin:`0% 10% 0% 0%`, padding:`0% 8% 0% 0%`, border:`3px solid`}} ref={register({ required: true })} />
+                    {errors.drivers_email && errors.drivers_email.type === "required" && (
+                        <p>This is required</p>
+                    )}
 
-            <label>Email:</label>
-            <input name="drivers_email" type="email" ref={register({ required: true })} />
-            {errors.drivers_email && errors.drivers_email.type === "required" && (
-                <p>This is required</p>
-            )}
-
-            <label>Password:</label>
-            <input name="password" type="password" ref={register({ required: true })} />
-            {errors.password && errors.password.type === "required" && (
-                <p>This is required</p>
-            )}
-
-            <button type="submit">Submit</button>
-        </form>
+                    <input name="password" type="password" placeholder="Password" style={{margin:`0% 0% 0% 10%`, padding:`0% 0% 0% 8%`, border:`3px solid`}} ref={register({ required: true })} />
+                    {errors.password && errors.password.type === "required" && (
+                        <p>This is required</p>
+                    )}
+                </div>
+                <div style={{margin: `0% 0% 12%`}}>
+                    <button type="submit" style={{width: `38%`, padding: `4% 0%`, background: `rgb(182, 194, 170`, border: `3px solid`}}>Submit</button>
+                </div>
+            </DriverForm>
+        </div>
     )
 }
 
